a8a1ffb21f2a591e4ef6a4463e03856687fcaf7c
ZhangZhihuiAAA <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-17 23:06:23 +0800

    Create pd_index.py

---
+++ pd_index.py
@@ -0,0 +1,5 @@
+It is a best practice when performing exploratory data analysis
+to first load the data and explore it using queries / Boolean
+selection. Then create an index if your data naturally supports
+one, or if you do require the increased speed.
+
