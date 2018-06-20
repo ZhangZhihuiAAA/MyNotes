bf4d5143c2173e82a25538fe36aac54ca4102889
ZhangZhihuiAAA <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-20 21:34:14 +0800

    Update pd_Categorical_add_order.py

--- pandas/pd_Categorical_add_order.py
+++ pandas/pd_Categorical_add_order.py
@@ -6,3 +6,13 @@ Categories (3, object): [a, b, c]
 >>> cat
 [a, b, c, a]
 Categories (3, object): [a < b < c]
+
+
+>>> cat = pd.Categorical(['a', 'b', 'c', 'a'], categories=['a', 'b', 'c'])
+>>> cat
+[a, b, c, a]
+Categories (3, object): [a, b, c]
+>>> cat.as_ordered(inplace=True)
+>>> cat
+[a, b, c, a]
+Categories (3, object): [a < b < c]
