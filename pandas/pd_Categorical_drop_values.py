31975f0f9d2866a70181f753b5639eeb9eeec95a
ZhangZhihuiAAA <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-20 22:30:22 +0800

    Create pd_Categorical_drop_values.py

---
+++ pandas/pd_Categorical_drop_values.py
@@ -0,0 +1,9 @@
+>>> cat
+[a, b, c, a]
+Categories (4, object): [a < b < c]
+>>> cat.get_values()
+array(['a', 'b', 'c', 'a'], dtype=object)
+>>> cat = pd.Categorical(np.delete(cat.get_values(), 1), categories=cat.categories, ordered=True)
+>>> cat
+[a, c, a]
+Categories (3, object): [a < b < c]
