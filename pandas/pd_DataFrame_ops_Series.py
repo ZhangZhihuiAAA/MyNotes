34a1998aa21acb5fd9653abb46ef519a7baa4ac5
ZhangZhihuiAAA <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-21 22:30:12 +0800

    Update pd_DataFrame_ops_Series.py

--- pandas/pd_DataFrame_ops_Series.py
+++ pandas/pd_DataFrame_ops_Series.py
@@ -35,3 +35,19 @@ Name: 0, dtype: float64
 2 NaN NaN NaN NaN NaN
 3 NaN NaN NaN NaN NaN
 4 NaN NaN NaN NaN NaN
+>>> s2 = s[1:3]
+>>> s2['E'] = 0
+>>> df + s2
+    A         B         C   D   E
+0 NaN  0.957887 -1.038877 NaN NaN
+1 NaN  1.872349 -0.426531 NaN NaN
+2 NaN  1.725378  0.487751 NaN NaN
+3 NaN  0.707856  0.833478 NaN NaN
+4 NaN  0.107101  1.149587 NaN NaN
+>>> s2 + df
+    A         B         C   D   E
+0 NaN  0.957887 -1.038877 NaN NaN
+1 NaN  1.872349 -0.426531 NaN NaN
+2 NaN  1.725378  0.487751 NaN NaN
+3 NaN  0.707856  0.833478 NaN NaN
+4 NaN  0.107101  1.149587 NaN NaN
